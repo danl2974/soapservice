30fbfcb34527efbfe1ae141dd807e0626277cf27
DLubin <DLubin@ABO007.welco.local>
2014-05-28 18:52:28 -0400

    May 28 6:52PM

--- SoapService/src/com/dl2974/soapws/SoapServiceImpl.java
+++ SoapService/src/com/dl2974/soapws/SoapServiceImpl.java
@@ -6,8 +6,18 @@ import javax.jws.WebService;
 public class SoapServiceImpl implements ISoapService {
 	
 	 public String tester(String name) {
+		 
 	  return "Tester Response: " + name;
+	  
 	 }
+	 
+	 public String power(String number) {
+		 
+		  double result = Math.pow(Double.valueOf(number), 2.0);
+		  return String.valueOf(result);
+		  
+		 }	 
+	 
 
 
 
